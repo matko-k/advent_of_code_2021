f3a4a00913cc23ce64a409b9e196d4118792507f
matko <m.kusenic@incubedit.com>
2021-11-30 16:11:15 +0100

    Prepare AOC DailyTask class

---
+++ DailyTaskTemplate/daily_task_XX.cpp
@@ -0,0 +1,6 @@
+#include "daily_task_XX.h"
+
+void DailyTaskXX::calculate()
+{
+    std::ifstream file("/home/incubed/advent_of_code_2021/sources/daily_task_XX_input.txt");
+}
---
+++ DailyTaskTemplate/daily_task_XX.h
@@ -0,0 +1,13 @@
+#ifndef AOC2021_DAILY_TASK_XX_H
+#define AOC2021_DAILY_TASK_XX_H
+
+#include <DailyTask/DailyTask.h>
+
+class DailyTaskXX : DailyTask {
+public:
+  DailyTaskXX(std::string task_name) : DailyTask(task_name){};
+
+  virtual void calculate();
+};
+
+#endif // AOC2021_DAILY_TASK_XX_H
